8b6575ee99d4b9e390a9b037589387297e585796
杨海涛 <516330797@qq.com>
2023-07-27 20:59:52 +0800

    feat: complete challenge

--- src/main/java/com/jirengu/PrimitiveDataTypesRange.java
+++ src/main/java/com/jirengu/PrimitiveDataTypesRange.java
@@ -2,30 +2,30 @@ package com.jirengu;
 
 public class PrimitiveDataTypesRange {
     // byte类型能表示的最大值/最小值
-    public static byte MAX_BYTE = 1;
-    public static byte MIN_BYTE = 1;
+    public static byte MAX_BYTE = Byte.MAX_VALUE;
+    public static byte MIN_BYTE = Byte.MIN_VALUE;
 
     // short类型能表示的最大值/最小值
-    public static short MAX_SHORT = 1;
-    public static short MIN_SHORT = 1;
+    public static short MAX_SHORT = Short.MAX_VALUE;
+    public static short MIN_SHORT = Short.MIN_VALUE;
     // int类型能表示的最大值/最小值
-    public static int MAX_INT = 1;
-    public static int MIN_INT = 1;
+    public static int MAX_INT = Integer.MAX_VALUE;
+    public static int MIN_INT = Integer.MIN_VALUE;
     // long类型能表示的最大值/最小值
-    public static long MAX_LONG = 1;
-    public static long MIN_LONG = 1;
+    public static long MAX_LONG = Long.MAX_VALUE;
+    public static long MIN_LONG = Long.MIN_VALUE;
 
     // float类型能表示的最大值/最小值
-    public static float MAX_FLOAT = 1.0f;
-    public static float MIN_FLOAT = 1.0f;
+    public static float MAX_FLOAT = Float.MAX_VALUE;
+    public static float MIN_FLOAT = Float.MIN_VALUE;
 
     // double类型能表示的最大值/最小值
-    public static double MAX_DOUBLE = 1.0;
-    public static double MIN_DOUBLE = 1.0;
+    public static double MAX_DOUBLE = Double.MAX_VALUE;
+    public static double MIN_DOUBLE = Double.MIN_VALUE;
 
     // char类型能表示的最大值/最小值？
-    public static char MAX_CHAR = 1;
-    public static char MIN_CHAR = 1;
+    public static char MAX_CHAR = Character.MAX_VALUE;
+    public static char MIN_CHAR = Character.MIN_VALUE;
 
     public static void main(String[] args) {
         System.out.println("Max byte: "+ MAX_BYTE);
